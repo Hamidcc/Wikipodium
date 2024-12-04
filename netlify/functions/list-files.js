7ef1d4c9d7e3bc56105e2966bdcef21e836526b3
Hamidcc <143912530+Hamidcc@users.noreply.github.com>
2024-12-03 20:21:34 -0600

    Update list-files.js

--- netlify/functions/list-files.js
+++ netlify/functions/list-files.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async function(event, context) {
-  const dirPath = path.join(__dirname, '../../wiki'); // Path to your HTML files folder
+  const dirPath = path.resolve(__dirname, '../../wiki/')
   const files = fs.readdirSync(dirPath); // List files in the folder
   
   const htmlFiles = files.filter(file => file.endsWith('.html')).map(file => file);
